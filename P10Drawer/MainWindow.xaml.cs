fea7b8eb02ccd0233ad18e6819e59b5bf519eac9
vanichka <ivan.kovalenko8080@gmail.com>
2023-11-15 16:41:08 +0100

    Added eraser.

--- P10Drawer/MainWindow.xaml.cs
+++ P10Drawer/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -19,18 +18,23 @@ namespace P10Drawer
         private int Cols { get; } = 128;
         // Размер пикселя
         private int Size { get; } = 5;
-
+        // Вулючена ли инверсия
         private bool inversion = false;
+        // Состояние курсора
+        private enum CURSOR
+        {
+            Brush,
+            Eraser
+        }
+        private CURSOR cursor = CURSOR.Brush;
 
-        // Включенные клавиши
-        private Dictionary<int, int> pixelInformation = new Dictionary<int, int>();
         public MainWindow()
         {
             InitializeComponent();
             CreateGridBySize(Cols, Rows);
         }
 
-        public void CreateGridBySize(int cols, int rows) 
+        public void CreateGridBySize(int cols, int rows)
         {
             for (int i = 0; i < cols; i++)
             {
@@ -52,6 +56,7 @@ namespace P10Drawer
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
+
                 Point mousePosition = e.GetPosition(drawCanvas);
 
                 if (mousePosition.X >= 0 && mousePosition.X <= drawCanvas.ActualWidth - 1 &&
@@ -60,34 +65,47 @@ namespace P10Drawer
                     int column = (int)(mousePosition.X / (drawCanvas.ActualWidth / Cols));
                     int row = (int)(mousePosition.Y / (drawCanvas.ActualHeight / Rows));
 
-                    // запись координат, (cols * 1000 + row)
-                    if (!Keys.Contains((row * 1000) + column)) Keys.Add((row * 1000) + column);
-
                     Rectangle rectangle;
 
-                    if (!inversion)
-                        rectangle = new Rectangle
-                        {
-                            Width = drawCanvas.ActualWidth / Cols,
-                            Height = drawCanvas.ActualHeight / Rows,
-                            Fill = Brushes.White,
-                            Stroke = Brushes.White,
-                            StrokeThickness = 1
-                        };
-                    else
-                        rectangle = new Rectangle
-                        {
-                            Width = drawCanvas.ActualWidth / Cols,
-                            Height = drawCanvas.ActualHeight / Rows,
-                            Fill = Brushes.Black,
-                            Stroke = Brushes.Black,
-                            StrokeThickness = 1
-                        };
+                    if (cursor == CURSOR.Brush)
+                    {
+                        // запись координат, (cols * 1000 + row) при условии, что таких пока нет
+                        if (!Keys.Contains((row * 1000) + column)) Keys.Add((row * 1000) + column);
 
-                    Canvas.SetLeft(rectangle, column * rectangle.Width);
-                    Canvas.SetTop(rectangle, row * rectangle.Height);
+                        if (!inversion)
+                            rectangle = new Rectangle
+                            {
+                                Width = drawCanvas.ActualWidth / Cols,
+                                Height = drawCanvas.ActualHeight / Rows,
+                                Fill = Brushes.White,
+                                Stroke = Brushes.White,
+                                StrokeThickness = 1
+                            };
+                        else
+                            rectangle = new Rectangle
+                            {
+                                Width = drawCanvas.ActualWidth / Cols,
+                                Height = drawCanvas.ActualHeight / Rows,
+                                Fill = Brushes.Black,
+                                Stroke = Brushes.Black,
+                                StrokeThickness = 1
+                            };
 
-                    drawCanvas.Children.Add(rectangle);
+                        Canvas.SetLeft(rectangle, column * rectangle.Width);
+                        Canvas.SetTop(rectangle, row * rectangle.Height);
+
+                        drawCanvas.Children.Add(rectangle);
+                    }
+                    else if (cursor == CURSOR.Eraser)
+                    {
+                        // удаление координат (cols * 1000 + row) при условии, что такие уже есть
+                        if (Keys.Contains((row * 1000) + column)) Keys.Remove((row * 1000) + column);
+
+                        if (drawCanvas.InputHitTest(mousePosition) is UIElement element)
+                        {
+                            drawCanvas.Children.Remove(element);
+                        }
+                    }
                 }
             }
         }
@@ -119,13 +137,13 @@ namespace P10Drawer
         // отправка данных
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
-            if (Keys.Count > 0) 
+            if (Keys.Count > 0)
             {
                 string answer = string.Empty;
                 int tmp;
-                for (int i = 0; i < Cols; i++) 
+                for (int i = 0; i < Cols; i++)
                 {
-                    for (int j = 0; j < Rows; j++) 
+                    for (int j = 0; j < Rows; j++)
                     {
                         tmp = (i * 1000) + j;
                         if (Keys.Contains(tmp))
@@ -133,7 +151,7 @@ namespace P10Drawer
                             if (!inversion) answer += "1";
                             else answer += "0";
                         }
-                        else 
+                        else
                         {
                             if (!inversion) answer += "0";
                             else answer += "1";
@@ -171,6 +189,11 @@ namespace P10Drawer
                 }
                 drawCanvas.Background = Brushes.Black;
             }
+        }
+
+        private void EraserButton_Click(object sender, RoutedEventArgs e)
+        {
+            cursor = CURSOR.Eraser;
         }
     }
 }
