edee9d7d4b3a732d4e3469f386cb563321f21d8f
vanichka <ivan.kovalenko8080@gmail.com>
2023-11-16 22:54:49 +0100

    Have added visualizer model in Resize Window.

--- P10Drawer/ResizeWindow.xaml.cs
+++ P10Drawer/ResizeWindow.xaml.cs
@@ -24,19 +24,54 @@ namespace P10Drawer
             InitializeComponent();
         }
 
-        private void WidhtSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        // Вызываем метод Slider_ValueChanged после того, как visualizeCanvas загрузился,
+        // чтобы убедиться, что его размеры уже определены
+        private void VisualizeCanvas_Loaded(object sender, RoutedEventArgs e)
         {
-            WidthCount.Text = Math.Round(WidhtSlider.Value).ToString();
+            Slider_ValueChanged(null, null);
         }
 
-        private void HeightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        bool isFirst = true;
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            HeightCount.Text = Math.Round(HeightSlider.Value).ToString();
+            if (WidthSlider != null) WidthCount.Text = Math.Round(WidthSlider.Value).ToString();
+            if (HeightSlider != null) HeightCount.Text = Math.Round(HeightSlider.Value).ToString();
+
+            if (visualizeCanvas != null && visualizeCanvas.Children.Count > 0) visualizeCanvas.Children.Clear();
+
+            if (WidthSlider != null && HeightSlider != null && visualizeCanvas != null)
+            {
+                double rectWidth = 32;
+                double rectHeight = 16;
+                double canvasWidth = visualizeCanvas.ActualWidth;
+                double canvasHeight = visualizeCanvas.ActualHeight;
+
+                double totalRectsWidth = Math.Round(WidthSlider.Value) * rectWidth;
+                double totalRectsHeight = Math.Round(HeightSlider.Value) * rectHeight;
+
+                double startX = (canvasWidth - totalRectsWidth) / 2;
+                double startY = (canvasHeight - totalRectsHeight) / 2;
+
+                for (int i = 0; i < (int)Math.Round(WidthSlider.Value); i++)
+                {
+                    for (int j = 0; j < (int)Math.Round(HeightSlider.Value); j++)
+                    {
+                        Rectangle newRect = new Rectangle()
+                        {
+                            Height = rectHeight,
+                            Width = rectWidth,
+                            Fill = Brushes.Black,
+                            Margin = new Thickness(startX + i * (rectWidth + 3), startY + j * (rectHeight + 3), 0, 0)
+                        };
+                        visualizeCanvas.Children.Add(newRect);
+                    }
+                }
+            }
         }
 
         private void ButtonConfirm_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow.Cols = (int)(Math.Round(WidhtSlider.Value) * 32);
+            MainWindow.Cols = (int)(Math.Round(WidthSlider.Value) * 32);
             MainWindow.Rows = (int)(Math.Round(HeightSlider.Value) * 16);
             DialogResult = true;
             this.Close();
