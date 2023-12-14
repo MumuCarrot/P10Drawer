293e4b3729c8a8ae488265a201d6e84a0f0b249d
vanichka <ivan.kovalenko8080@gmail.com>
2023-12-14 16:50:48 +0100

    Comment update (first part)

--- P10Drawer/MainWindow.xaml.cs
+++ P10Drawer/MainWindow.xaml.cs
@@ -14,30 +14,67 @@ namespace P10Drawer
     /// </summary>
     public partial class MainWindow : Window
     {
-        // Колличество рядов
-        public static int Rows { get; set; } = 16;
-        // Колличество строк
-        public static int Cols { get; set; } = 32;
-        // Размер пикселя
-        private int Size { get; set; } = 5;
-        // Включена ли инверсия
-        private bool inversion = false;
-        // Состояние курсора
-        private enum CURSOR
-        {
-            Brush,
-            Eraser
-        }
-        private CURSOR cursor = CURSOR.Brush;
-
-        // Переменная для сохранения месседжа
-        private string Save { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Initialisation of the MainWindow
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        // Variables of the P10Drawer region
+        #region Variables of the P10Drawer
+        /// <summary>
+        /// Rows counter
+        /// </summary>
+        public static int Rows { get; set; } = 16;
+        /// <summary>
+        /// Columns counter
+        /// </summary>
+        public static int Cols { get; set; } = 32;
+        /// <summary>
+        /// Pixel actual size
+        /// </summary>
+        private int Size { get; set; } = 5;
+        /// <summary>
+        /// Inversion
+        /// </summary>
+        private bool inversion = false;
+        // Cursor state region
+        #region Cursor states
+        /// <summary>
+        /// Cursor state enum | Variation of the cursour
+        /// </summary>
+        private enum CURSOR
+        {
+            Brush,
+            Eraser
+        }
+        /// <summary>
+        /// Cursor actual stat
+        /// </summary>
+        private CURSOR cursor = CURSOR.Brush;
+        #endregion // EO Cursor state region
+        /// <summary>
+        /// Is nedded to upload previous action
+        /// </summary>
+        private string Save { get; set; } = string.Empty;
+        #endregion // EO Variables of the P10Drawer
+
+        // Methods of the P10Drawer region
+        #region Methods of the P10Drawer
+
+        // Helper methods region
+        #region Helper methods region
+        /// <summary>
+        /// Creates and resizing drawing grid
+        /// </summary>
+        /// <param name="resizeWindow">
+        /// Should be used if it resizing
+        /// </param>
+        /// <param name="sizeChange">
+        /// Should be used if it save load when size of pixels needed ot be changed
+        /// </param>
         public void CreateGridBySize(bool resizeWindow = false, bool sizeChange = false)
         {
             if (resizeWindow)
@@ -68,8 +105,50 @@ namespace P10Drawer
             if (sizeChange)
                 RecreateFromSave(Save);
         }
+        /// <summary>
+        /// Parsing canvas to binary array
+        /// 0 - black
+        /// 1 - white
+        /// </summary>
+        private string GetCanvasInfo()
+        {
+            List<string> keys = new List<string>();
 
-        // Движение мышью в канвасе
+            foreach (Rectangle rec in drawCanvas.Children)
+            {
+                int x = (int)Canvas.GetTop(rec) / Size;
+                int y = (int)Canvas.GetLeft(rec) / Size;
+                if (!keys.Contains($"{x}" + "#" + $"{y}")) keys.Add($"{x}" + "#" + $"{y}");
+            }
+
+            string answer = string.Empty;
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    if (!inversion)
+                    {
+                        if (keys.Contains($"{i}" + "#" + $"{j}")) answer += "1";
+                        else answer += "0";
+                    }
+                    else if (inversion)
+                    {
+                        if (keys.Contains($"{i}" + "#" + $"{j}")) answer += "0";
+                        else answer += "1";
+                    }
+                }
+            }
+
+            return answer;
+        }
+        #endregion // EO Helper methods region
+
+        // Drawing region
+        #region Drawing region
+        /// <summary>
+        /// Drawing
+        /// </summary>
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -125,73 +204,81 @@ namespace P10Drawer
                 }
             }
         }
-
-        // захват курсора (вход)
+        /// <summary>
+        /// Begining of the mous capture
+        /// </summary>
         private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             drawCanvas.CaptureMouse();
         }
-
-        //захват курсора (выход)
+        /// <summary>
+        /// Release mouse capture
+        /// </summary>
         private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             drawCanvas.ReleaseMouseCapture();
         }
+        /// <summary>
+        /// Setts for the cursor status
+        /// </summary>
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is ToggleButton clickedButton)) return;
 
-        // смена курсора (вход)
+            if (clickedButton == BrushButton && BrushButton.IsChecked == true)
+            {
+                // Включить режим "Кисть"
+                cursor = CURSOR.Brush;
+                EraserButton.IsChecked = false; // Выключить режим "Ластик"
+                                                // Дополнительные действия для включения режима "Кисть"
+            }
+            else if (clickedButton == EraserButton && EraserButton.IsChecked == true)
+            {
+                // Включить режим "Ластик"
+                cursor = CURSOR.Eraser;
+                BrushButton.IsChecked = false; // Выключить режим "Кисть"
+                                               // Дополнительные действия для включения режима "Ластик"
+            }
+            else
+            {
+                // Если кнопка отжата, выполните соответствующие действия для выключения выбранного режима
+                cursor = CURSOR.Brush;
+                BrushButton.IsChecked = true;
+            }
+        }
+        #endregion // EO Drawing region
+
+        // Visual cursor chagings region
+        #region Visual cursor chages
+        /// <summary>
+        /// Changes cursor visualisation
+        /// </summary>
         private void Canvas_MouseEnter(object sender, MouseEventArgs e)
         {
             drawCanvas.Cursor = Cursors.Pen;
         }
-
-        // смена курсора (выход)
+        /// <summary>
+        /// Changes cursor visualisation
+        /// </summary>
         private void Canvas_MouseLeave(object sender, MouseEventArgs e)
         {
             drawCanvas.Cursor = Cursors.Arrow;
         }
+        #endregion // EO Visual cursor chages
 
-        // отправка данных
-        private void SendButton_Click(object sender, RoutedEventArgs e)
+        // Indirect impact on canvas region
+        #region Indirect impact on canvas
+        /// <summary>
+        /// Copies data to the clipboard
+        /// </summary>
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
         {
             Save = GetCanvasInfo();
             Clipboard.SetData(DataFormats.Text, (Object)Save);
         }
-
-        // информаци про текущее состояние объектов на канвасе
-        private string GetCanvasInfo()
-        {
-            List<string> keys = new List<string>();
-
-            foreach (Rectangle rec in drawCanvas.Children)
-            {
-                int x = (int)Canvas.GetTop(rec) / Size;
-                int y = (int)Canvas.GetLeft(rec) / Size;
-                if (!keys.Contains($"{x}" + "#" + $"{y}")) keys.Add($"{x}" + "#" + $"{y}");
-            }
-
-            string answer = string.Empty;
-
-            for (int i = 0; i < Rows; i++)
-            {
-                for (int j = 0; j < Cols; j++)
-                {
-                    if (!inversion)
-                    {
-                        if (keys.Contains($"{i}" + "#" + $"{j}")) answer += "1";
-                        else answer += "0";
-                    }
-                    else if (inversion)
-                    {
-                        if (keys.Contains($"{i}" + "#" + $"{j}")) answer += "0";
-                        else answer += "1";
-                    }
-                }
-            }
-
-            return answer;
-        }
-
-        // инверсия цветов
+        /// <summary>
+        /// Inverts all pixels black to white and white to black
+        /// </summary>
         private void InversionButton_Click(object sender, RoutedEventArgs e)
         {
             inversion = !inversion;
@@ -220,41 +307,20 @@ namespace P10Drawer
                 drawCanvas.Background = Brushes.Black;
             }
         }
-
-        // удаление всего
+        /// <summary>
+        /// Clears field to zero
+        /// </summary>
         private void DeleteAllButoon_Click(object sender, RoutedEventArgs e)
         {
             drawCanvas.Children.Clear();
         }
+        #endregion // EO Indirect impact on canvas region
 
-        // состояние мыши
-        private void ToggleButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (!(sender is ToggleButton clickedButton)) return;
-
-            if (clickedButton == BrushButton && BrushButton.IsChecked == true)
-            {
-                // Включить режим "Кисть"
-                cursor = CURSOR.Brush;
-                EraserButton.IsChecked = false; // Выключить режим "Ластик"
-                                                // Дополнительные действия для включения режима "Кисть"
-            }
-            else if (clickedButton == EraserButton && EraserButton.IsChecked == true)
-            {
-                // Включить режим "Ластик"
-                cursor = CURSOR.Eraser;
-                BrushButton.IsChecked = false; // Выключить режим "Кисть"
-                                               // Дополнительные действия для включения режима "Ластик"
-            }
-            else
-            {
-                // Если кнопка отжата, выполните соответствующие действия для выключения выбранного режима
-                cursor = CURSOR.Brush;
-                BrushButton.IsChecked = true;
-            }
-        }
-
-        // ресайз канваса
+        // Resize canvas region
+        #region Resize canvas
+        /// <summary>
+        /// Resizes canvas | Unites modules
+        /// </summary>
         private void ResizeButton_Click(object sender, RoutedEventArgs e)
         {
             ResizeWindow inputWindow = new ResizeWindow(); // Создание экземпляра нового окна
@@ -266,7 +332,9 @@ namespace P10Drawer
                 CreateGridBySize(resizeWindow: true);
             }
         }
-
+        /// <summary>
+        /// Recreates save
+        /// </summary>
         private void RecreateFromSave(string str)
         {
             if (drawCanvas != null)
@@ -310,7 +378,9 @@ namespace P10Drawer
                 }
             }
         }
-
+        /// <summary>
+        /// Resize slider value changed
+        /// </summary>
         private void SizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             Save = GetCanvasInfo();
@@ -318,7 +388,13 @@ namespace P10Drawer
 
             CreateGridBySize(sizeChange: true);
         }
+        #endregion // EO Resize canvas region
 
+        // ToolBar methods region
+        #region ToolBar methods
+        /// <summary>
+        /// Drag and move
+        /// </summary>
         private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -326,21 +402,30 @@ namespace P10Drawer
                 this.DragMove();
             }
         }
-
+        /// <summary>
+        /// Close button
+        /// </summary>
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
+        /// <summary>
+        /// Minimization button
+        /// </summary>
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
         }
-
-        private void MaxemizeButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Maximization button
+        /// </summary>
+        private void MaximizationButton_Click(object sender, RoutedEventArgs e)
         {
             if (WindowState == WindowState.Maximized) WindowState = WindowState.Normal;
             else WindowState = WindowState.Maximized;
         }
+        #endregion // EO ToolBar methods
+
+        #endregion // EO Methods of the P10Drawer
     }
 }
